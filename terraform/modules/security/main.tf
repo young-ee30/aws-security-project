1880b16c7938adfe63f19081e534003931d11d56
level-high <jsooha7@gmail.com>
2026-03-19 14:38:11 +0900

    cloudfront waf terraform test2

--- terraform/modules/security/main.tf
+++ terraform/modules/security/main.tf
@@ -16,7 +16,7 @@ data "aws_ec2_managed_prefix_list" "cloudfront" {
 
 resource "aws_security_group" "alb" {
   name        = "${var.name_prefix}-alb-sg"
-  description = "ALB accepts traffic only from CloudFront edge nodes"
+  description = "ALB security group"
   vpc_id      = var.vpc_id
 
   ingress {
