171a96489284b941ae35861ab208739896f21ff2
young-ee03 <eyoungblll30@gmail.com>
2026-03-24 14:54:07 +0900

    remove-agentation

--- controlplane/web/src/main.tsx
+++ controlplane/web/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { Agentation } from 'agentation'
 import App from './App'
 import './index.css'
 
@@ -9,7 +8,6 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
-      {import.meta.env.DEV && <Agentation />}
     </BrowserRouter>
   </React.StrictMode>,
 )
